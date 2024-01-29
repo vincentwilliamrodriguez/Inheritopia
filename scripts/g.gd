95e8cb0c8e5ba84445eb66c95b1033f69ff2afa4
Vincent William Rodriguez <vincentwilliamr@gmail.com>
2024-01-29 16:27:00 +0800

    Add Event class

--- scripts/g.gd
+++ scripts/g.gd
@@ -9,13 +9,21 @@ var breed_lookup = []
 var neighbors_lookup = []
 
 var traits = ['y', 't', 'r']
+var events = []
 var genotypes = [["yy", "yY", "Yy", "YY"],
 				 ["tt", "tT", "Tt", "TT"],
 				 ["rr", "rR", "Rr", "RR"]]
 
 class Event:
+	var name: String
 	var affected_map: int
 	var affected_trait: int
+	var survival_chances: Array
+	
+	func initialize(inp_name, inp_trait, inp_chances):
+		name = inp_name
+		affected_trait = inp_trait
+		survival_chances = inp_chances
 
 func _ready():
 	# Initialize breed_lookup
@@ -39,8 +47,17 @@ func _ready():
 					   (new_x >= 0 and new_x < 4) and \
 					   (new_y >= 0 and new_y < 4):
 					neighbors_lookup[i].append(new_x + 4 * new_y)
-				
-
+	
+	# Initializing events
+	for i in 6:
+		events.append(Event.new())
+		
+	events[0].initialize("Storm", 1, [0.9, 0.7])
+	events[1].initialize("Waterlogging", 2, [0.7, 0.5])
+	events[2].initialize("Drought", 2, [0.5, 0.9])
+	events[3].initialize("Pest Invasion", 0, [0.4, 0.4])
+	events[4].initialize("Night", 1, [0.6, 0.9])
+	events[5].initialize("Fertility", 0, [1.0, 1.0])
 
 func _process(delta):
 	pass
